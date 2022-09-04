2fa534a5c7fa5585979050f9c4bdbc79467bcc15
bakerkhoder <bakerkhoder7@gmail.com>
2022-09-04 22:02:13 +0300

    adding the timer feature

--- Maze Js Game/game.js
+++ Maze Js Game/game.js
@@ -9,17 +9,49 @@ function myfunction(){
  var end= document.getElementById("end")
  var score_text=document.getElementById("status")
  var total_result=document.getElementsByTagName("h1")
+ var incomeTicker=60;
+ var username=prompt("Welcome to the maz game ,please enter your name")
  boxes.forEach((box)=>{
  box.addEventListener("mouseover",()=>{
-
+ 
  if(total_result[0].innerHTML=="game started"){ score-=10
      total_result[0].innerHTML="Hover over S to restart"
     textresult[0].innerHTML="YOU LOST"
  boxes.forEach(box=>{box.style.backgroundColor="red"})}})})
  
  start.onmouseover=()=>{
+  mytimer =setInterval(clock,1000)
+ function clock(){
+  if(incomeTicker>0){
+    incomeTicker--
+    textresult[0].innerHTML=incomeTicker
+    
+   }
+  if(incomeTicker<=0){
+    
+    textresult[0].innerHTML="YOU LOST"
+ boxes.forEach(box=>{box.style.backgroundColor="red"})
+  }
+  
+  end.onmouseover=()=>{
+  
+ if(textresult[0].style.backgroundColor!="red"&&total_result[0].innerHTML=="game started"){
+   
+ score+=5 
+ incomeTicker=60
+ clearInterval(mytimer)
+console.log(score)}
+ textresult[0].innerHTML="YOU WON"
+ if(score>=0){total_result[0].innerHTML="WINNER"}
+ else{total_result[0].innerHTML="LOSER"}
+ score_text.innerHTML="your score is : "+score;}
  
-  /*
+ 
+   }
+   //shil you won hek\
+   //wa2if timer
+
+   /*
     cheating message
   let isMouseHover = false
    let test = document.getElementById("game");
@@ -28,20 +60,12 @@ function myfunction(){
    textresult[0].innerHTML="cheating score wont be added"
    console.log(isMouseHover)
    }, false);*/
-   
- total_result[0].innerHTML="game started"   
- boxes.forEach(box=>{box.style.backgroundColor=" #eeeeee"}) }
-
- end.onmouseover=()=>{
   
- if(textresult[0].style.backgroundColor!="red"&&total_result[0].innerHTML=="game started"){
-   
- score+=5 
-console.log(score)}
- textresult[0].innerHTML="YOU WON"
- if(score>0){total_result[0].innerHTML="WINNER"}
- else{total_result[0].innerHTML="LOSER"}
- score_text.innerHTML="your score is : "+score;}
+ total_result[0].innerHTML="game started"   
+ boxes.forEach(box=>{box.style.backgroundColor=" #eeeeee"}) 
+ console.log('ee')}
+
+
  
  start.onclick=()=>{
  score=0
